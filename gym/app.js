563eb1ac6fe66ed1d444643bdadfcc199b92428a
JeanCardozo <144623024+JeanCardozo@users.noreply.github.com>
2024-09-09 15:32:55 -0500

    V2.9

--- gym/app.js
+++ gym/app.js
@@ -37,6 +37,7 @@ cron.schedule("0 0 * * *", () => {
 app.use(morgan("dev"));
 app.use(cors());
 
-app.listen(5000, () => {
-  console.log("Servidor corriendo en http://localhost:5000");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
